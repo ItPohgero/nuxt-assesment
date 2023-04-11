cc1ab688325877a54bb5aeba9cf07a97f94b7ba9
wahyuagusarifin <wahyu.agusarifin@ai.astra.co.id>
2023-04-11 13:40:14 +0700

    add card components - slider

---
+++ components/CardSlide.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="bg-black/80 h-72 w-[500px] my-20 grid grid-cols-2 relative">
+        <div class="absolute -top-6 h-72 w-56">
+            <img src="~assets/image/slideimg2.png" alt="movie" class="object-cover" />
+        </div>
+        <div />
+        <div class="pr-4 py-2">
+            <div class="flex items-center gap-2 mt-4">
+                <div>
+                    <img src="~assets/image/start.png" alt="start" />
+                </div>
+                <div class="font-bold text-white">7.3</div>
+            </div>
+            <h1 class="text-xl font-bold text-white">Space Sweepers</h1>
+            <div class="flex items-center gap-2 text-white mt-2">
+                <div>2021</div>
+                <div class="flex items-center gap-2">
+                    <div class="w-2 h-2 bg-slate-300 rounded-full"/>
+                    <div>Drama</div>
+                </div>
+            </div>
+            <div class="text-xs text-white mt-4">When the crew of a space junk collector ship called The Victory discovers a humanoid robot named Dorothy that's known to be a weapon of mass destruction, they get involved in a risky business deal which puts their lives at stake.</div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,7 @@
 <template>
+    <div class="flex justify-between items-center">
+        <CardSlide />
+    </div>
     <div class="bg-slate-700 py-2 w-full">
         asd
     </div>
